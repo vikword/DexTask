09360f826965cdfa21386acb2c6cbae22c3175a1
vikword <vikword@gmail.com>
2021-07-13 09:01:06 +0300

    Добавьте файлы проекта.

---
+++ CustomType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexTask
+{
+    class CustomType
+    {
+        public string Lines { get; private set; }
+        public int Numbers { get; private set; }
+        public DateTime DateTime { get; private set; }
+        public bool BoolFlag { get; private set; }
+
+        private static readonly Random _rand = new Random();
+
+        public override string ToString()
+        {
+            return $"Строка: {Lines}\tЧисло: {Numbers}\tДата: {DateTime}\tBool: {BoolFlag}";
+        }
+
+        public static CustomType GeneratingObjects()
+        {
+            bool flag = false;
+            if (_rand.Next(0, 100) % 2 == 0)
+            {
+                flag = true;
+            }
+            return new CustomType()
+            {
+                Lines = Guid.NewGuid().ToString("n").Substring(0, 8),
+                Numbers = _rand.Next(100, 130),
+                DateTime = (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(_rand.Next(1000000000, 1999999999)),
+                BoolFlag = flag
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DexTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomType[] obj = new CustomType[100];
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i] = CustomType.GeneratingObjects();
+            }
+
+            var WhereOrder = obj.Where(n => n.BoolFlag == true && n.Lines[0] == 'c')
+                                .OrderBy(u => u.Numbers);
+            foreach (var n in WhereOrder)
+            {
+                Console.WriteLine(n);
+            }
+
+            var group = obj.GroupBy(c => c.BoolFlag);
+            foreach (var c in group)
+            {
+                Console.WriteLine($"{c.Key} - количество: {c.Count()}\n");
+            }
+
+            var all = obj.All(s => s.Numbers > 99);
+            Console.WriteLine($"Все элементы коллекции obj.Numbers больше: 127 - {all}\n");
+
+            var any = obj.Any(u => u.Numbers > 140);
+            Console.WriteLine($"Есть элемент в коллекции obj.Numbers больше: 140 - {any}\n");
+
+            var sum = obj.Sum(s => s.Numbers);
+            Console.WriteLine($"Сумма чисел obj.Numbers {sum}\n");
+
+            var min = obj.Min(s => s.Numbers);
+            Console.WriteLine($"Минимальное значение obj.Numbers {min}\n");
+
+            var max = obj.Max(s => s.Numbers);
+            Console.WriteLine($"Максимальное значение obj.Numbers {max}\n");
+            Console.ReadKey();
+        }
+    }
+}
